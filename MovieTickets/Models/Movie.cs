ca6d8cb8ecd6416e3ebbc184df0d5de10acbffcf
Ivan Iliev <S1809010654@sd.uni-vt.bg>
2022-01-21 13:35:07 +0200

    Creating the models for the movie tickets app. Adding the AppDbContext

---
+++ MovieTickets/Models/Movie.cs
@@ -0,0 +1,25 @@
+﻿using MovieTickets.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieTickets.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public string Descripiton { get; set; }
+        public double Price { get; set; }
+        public string ImageURL { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndtDate { get; set; }
+        public MovieCategory MovieCategory { get; set; }
+
+
+    }
+}
--- MovieTickets/Startup.cs
+++ MovieTickets/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MovieTickets.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,6 +24,8 @@ namespace MovieTickets
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // DbContext config
+            services.AddDbContext<AppDbContext>();  
             services.AddControllersWithViews();
         }
 
